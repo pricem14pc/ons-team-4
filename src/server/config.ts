8806acc7440911fd61627acbc4abbb79ab7d0b1e
jamie kerr <jamiestephenkerr@gmail.com>
2023-06-20 12:36:09 +0100

    remove console output

--- src/server/config.ts
+++ src/server/config.ts
@@ -10,9 +10,6 @@ export function getConfiguration(): Config {
     BLAISE_API_URL,
   } = process.env;
 
-  console.debug('Api url', BLAISE_API_URL);
-  console.debug('Port', PORT);
-
   return {
     BlaiseApiUrl: BLAISE_API_URL === undefined ? 'http://localhost:5011' : BLAISE_API_URL, // Rest Api url
     BuildFolder: '../build', // where ever the react built package is
