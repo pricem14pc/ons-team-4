9a4011b4aac4ae20310da3643f16bf97063d32e0
kristian4res <57638182+Enigma-cloud@users.noreply.github.com>
2023-07-10 16:56:40 +0100

    added tests and config for environment

--- src/server/configuration.test.ts
+++ src/server/configuration.test.ts
@@ -3,33 +3,43 @@ import Configuration from "./configuration";
 
 describe('Configuration file tests', () => {
     const emptyEnv = process.env;
+    const blaiseApiUrl = "http://localhost:80";
+    const buildFolder = "../build";
+    const port = 5000;
 
     beforeEach(() => {
         jest.resetModules() // Most important - it clears the cache
-        process.env = {...emptyEnv};
+        process.env['BLAISE_API_URL'] = blaiseApiUrl
+        process.env['PORT'] = port.toString()
       });
 
-    // ensure other test files are not pullted with environment variable setup
+    afterEach(() => {
+        process.env = {...emptyEnv};
+    })
+
+    // ensure other test files are not pullted with environment variable setup  
     afterAll(() => {
         process.env = {...emptyEnv};
     })
-      
-    it("should populate the properties with values from environement variables when they exist in the environment variables", () => { 
-        // arrange
-        const blaiseApiUrl = "http://localhost:80";
-        process.env['BLAISE_API_URL'] = blaiseApiUrl
 
+    it("should populate the properties with values from environement variables when they exist in the environment variables", () => {         
         // act
         const sut = new Configuration();
 
         // assert
-        expect(sut.BlaiseApiUrl).toEqual(blaiseApiUrl)
+        expect(sut.BlaiseApiUrl).toEqual(blaiseApiUrl);
+        expect(sut.BuildFolder).toEqual(buildFolder);
+        expect(sut.Port).toEqual(port);
     });
 
     it("should throw an error if the BLAISE_API_URL does not exist in the environment variables", () => { 
+        // arrange
+        process.env['BLAISE_API_URL'] = undefined;
+        const configuration = () => {new Configuration();}
+        
         // act && assert
-
-        expect(() => {new Configuration();}).toThrowError('BLAISE_API_URL not found in environment variables')
+        expect(configuration).toThrowError(ReferenceError)
+        expect(configuration).toThrowError('BLAISE_API_URL not found in environment variables')
     });   
 
     it.each(['', ' ', '  '])("should throw an error if the BLAISE_API_URL is empty", (value) => { 
@@ -39,4 +49,24 @@ describe('Configuration file tests', () => {
         // act && assert
         expect(() => {new Configuration();}).toThrowError('BLAISE_API_URL is an empty string')
     });  
+
+    it("should throw an error if the PORT does not exist in the environment variables", () => { 
+        // arrange
+        process.env['PORT'] = undefined;
+        const configuration = () => {new Configuration();}
+
+        // act && assert
+        expect(configuration).toThrowError(ReferenceError)
+        expect(configuration).toThrowError('PORT not found in environment variables')
+    });
+    
+    it.each(['', 'NotNumber', 'eight'])("should throw an error if the PORT not number", (value) => { 
+        // arrange
+        process.env['PORT'] = value;
+        const configuration = () => {new Configuration();}
+        
+        // act && assert
+        expect(configuration).toThrowError(TypeError)
+        expect(configuration).toThrowError('PORT is not a valid number')
+    });
 });
--- src/server/configuration.ts
+++ src/server/configuration.ts
@@ -3,21 +3,34 @@ import { IConfiguration } from "./interfaces/configuration.interface";
 export default class Configuration implements IConfiguration {
 
     BlaiseApiUrl: string;
+    BuildFolder: string;
+    Port: number;
 
     constructor() {
         const {
             BLAISE_API_URL,
+            PORT
           } = process.env;
 
           if (BLAISE_API_URL === undefined){
-            throw new Error('BLAISE_API_URL not found in environment variables');
+            throw new ReferenceError('BLAISE_API_URL not found in environment variables');
           }
 
-          if (BLAISE_API_URL === ''){
+          if (BLAISE_API_URL.trim() === ''){
             throw new Error('BLAISE_API_URL is an empty string');
           }
 
+          if (PORT === undefined){
+            throw new ReferenceError('PORT not found in environment variables');
+          }
+
+          if (isNaN(+PORT) || +PORT === 0){
+            throw new TypeError('PORT is not a valid number');
+          }
+
           this.BlaiseApiUrl = BLAISE_API_URL;
+          this.BuildFolder = '../build'
+          this.Port = +PORT
     }
 }
 
