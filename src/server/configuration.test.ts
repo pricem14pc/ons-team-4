4e87123d2d2d51ed8980549c36814aa6a20f635e
willst1 <thomas.wills@ons.gov.uk>
2023-07-11 14:03:07 +0100

    added serverpark and VMWebUrl to configuration.ts

--- src/server/configuration.test.ts
+++ src/server/configuration.test.ts
@@ -1,66 +1,111 @@
-import Configuration from "./configuration";
+import Configuration from './configuration';
 
+/* eslint-disable no-new */
 
 describe('Configuration file tests', () => {
-    const emptyEnv = process.env;
-    const blaiseApiUrl = "http://localhost:80";
-    const buildFolder = "../build";
-    const port = 5000;
+  const emptyEnv = process.env;
+  const blaiseApiUrl = 'rest.api.blaise.com';
+  const buildFolder = '../build';
+  const port = 5000;
+  const serverPark = 'gusty';
+  const externalWebUrl = 'cati.blaise.com';
 
-    beforeEach(() => {
-        process.env['BLAISE_API_URL'] = blaiseApiUrl
-        process.env['PORT'] = port.toString()
-      });
+  beforeEach(() => {
+    process.env['BLAISE_API_URL'] = blaiseApiUrl;
+    process.env['PORT'] = port.toString();
+    process.env['SERVER_PARK'] = serverPark;
+    process.env['VM_EXTERNAL_WEB_URL'] = externalWebUrl;
+  });
 
-    afterEach(() => {
-        process.env = {...emptyEnv};
-    })
+  afterEach(() => {
+    process.env = { ...emptyEnv };
+  });
 
-    it("should populate the properties with values from environement variables when they exist in the environment variables", () => {         
-        // act
-        const sut = new Configuration();
+  it('should populate the properties with values from environement variables when they exist in the environment variables', () => {
+    // act
+    const sut = new Configuration();
 
-        // assert
-        expect(sut.BlaiseApiUrl).toEqual(blaiseApiUrl);
-        expect(sut.BuildFolder).toEqual(buildFolder);
-        expect(sut.Port).toEqual(port);
-    });
+    // assert
+    expect(sut.BlaiseApiUrl).toEqual(blaiseApiUrl);
+    expect(sut.BuildFolder).toEqual(buildFolder);
+    expect(sut.Port).toEqual(port);
+    expect(sut.ServerPark).toEqual(serverPark);
+    expect(sut.ExternalWebUrl).toEqual(externalWebUrl);
+  });
 
-    it("should throw an error if the BLAISE_API_URL does not exist in the environment variables", () => { 
-        // arrange
-        process.env['BLAISE_API_URL'] = undefined;
-        const configuration = () => {new Configuration();}
-        
-        // act && assert
-        expect(configuration).toThrowError(ReferenceError)
-        expect(configuration).toThrowError('BLAISE_API_URL was not found in environment variables')
-    });   
+  it('should throw an error if the BLAISE_API_URL does not exist in the environment variables', () => {
+    // arrange
+    process.env['BLAISE_API_URL'] = undefined;
+    const configuration = () => { new Configuration(); };
 
-    it.each(['', ' ', '  '])("should throw an error if the BLAISE_API_URL is empty", (value) => { 
-        // arrange
-        process.env['BLAISE_API_URL'] = value;
-        
-        // act && assert
-        expect(() => {new Configuration();}).toThrowError('BLAISE_API_URL is an empty string')
-    });  
+    // act && assert
+    expect(configuration).toThrowError(ReferenceError);
+    expect(configuration).toThrowError('BLAISE_API_URL was not found in environment variables');
+  });
 
-    it("should throw an error if the PORT does not exist in the environment variables", () => { 
-        // arrange
-        process.env['PORT'] = undefined;
-        const configuration = () => {new Configuration();}
+  it.each(['', ' ', '  '])('should throw an error if the BLAISE_API_URL is empty', (value) => {
+    // arrange
+    process.env['BLAISE_API_URL'] = value;
 
-        // act && assert
-        expect(configuration).toThrowError(ReferenceError)
-        expect(configuration).toThrowError('PORT was not found in environment variables')
-    });
-    
-    it.each(['', 'NotNumber', 'eight'])("should throw an error if the PORT is not number", (value) => { 
-        // arrange
-        process.env['PORT'] = value;
-        const configuration = () => {new Configuration();}
-        
-        // act && assert
-        expect(configuration).toThrowError(TypeError)
-        expect(configuration).toThrowError('PORT is not set to a valid number')
-    });
+    // act && assert
+    expect(() => { new Configuration(); }).toThrowError('BLAISE_API_URL is set to an empty string');
+  });
+
+  it('should throw an error if the PORT does not exist in the environment variables', () => {
+    // arrange
+    process.env['PORT'] = undefined;
+    const configuration = () => { new Configuration(); };
+
+    // act && assert
+    expect(configuration).toThrowError(ReferenceError);
+    expect(configuration).toThrowError('PORT was not found in environment variables');
+  });
+
+  it.each(['', 'NotNumber', 'eight'])('should throw an error if the PORT is not number', (value) => {
+    // arrange
+    process.env['PORT'] = value;
+    const configuration = () => { new Configuration(); };
+
+    // act && assert
+    expect(configuration).toThrowError(TypeError);
+    expect(configuration).toThrowError('PORT is not set to a valid number');
+  });
+
+  it('should throw an error if the SERVER_PARK does not exist in the environment variables', () => {
+    // arrange
+    process.env['SERVER_PARK'] = undefined;
+    const configuration = () => { new Configuration(); };
+
+    // act && assert
+    expect(configuration).toThrowError(ReferenceError);
+    expect(configuration).toThrowError('SERVER_PARK was not found in environment variables');
+  });
+
+  it.each(['', '  ', '   '])('should throw an error if SERVER_PARK is empty', (value) => {
+    // arrange
+    process.env['SERVER_PARK'] = value;
+    const configuration = () => { new Configuration(); };
+
+    // act && assert
+    expect(configuration).toThrowError('SERVER_PARK is set to an empty string');
+  });
+
+  it('should throw an error if the VM_EXTERNAL_WEB_URL does not exist in the environment variables', () => {
+    // arrange
+    process.env['VM_EXTERNAL_WEB_URL'] = undefined;
+    const configuration = () => { new Configuration(); };
+
+    // act && assert
+    expect(configuration).toThrowError(ReferenceError);
+    expect(configuration).toThrowError('VM_EXTERNAL_WEB_URL was not found in environment variables');
+  });
+
+  it.each(['', '  ', '   '])('should throw an error if VM_EXTERNAL_WEB_URL is empty', (value) => {
+    // arrange
+    process.env['VM_EXTERNAL_WEB_URL'] = value;
+    const configuration = () => { new Configuration(); };
+
+    // act && assert
+    expect(configuration).toThrowError('VM_EXTERNAL_WEB_URL is set to an empty string');
+  });
 });
--- src/server/configuration.ts
+++ src/server/configuration.ts
@@ -1,37 +1,61 @@
-import { IConfiguration } from "./interfaces/configuration.interface";
+import { IConfiguration } from './interfaces/configuration.interface';
 
 export default class Configuration implements IConfiguration {
+  BlaiseApiUrl: string;
 
-    BlaiseApiUrl: string;
-    BuildFolder: string;
-    Port: number;
-    //SERVER_PARK
-    //VM_EXTERNAL_WEB_URL
+  BuildFolder: string;
 
-    constructor() {
-        const {
-            BLAISE_API_URL,
-            PORT
-          } = process.env;
+  Port: number;
 
-          if (BLAISE_API_URL === undefined){
-            throw new ReferenceError('BLAISE_API_URL was not found in environment variables');
-          }
+  ServerPark: string;
 
-          if (BLAISE_API_URL.trim() === ''){
-            throw new Error('BLAISE_API_URL is an empty string');
-          }
+  ExternalWebUrl: string;
+  // VM_EXTERNAL_WEB_URL
 
-          if (PORT === undefined){
-            throw new ReferenceError('PORT was not found in environment variables');
-          }
+  constructor() {
+    const {
+      BLAISE_API_URL,
+      PORT,
+      SERVER_PARK,
+      VM_EXTERNAL_WEB_URL,
+    } = process.env;
 
-          if (isNaN(+PORT) || +PORT === 0){
-            throw new TypeError('PORT is not set to a valid number');
-          }
-
-          this.BlaiseApiUrl = BLAISE_API_URL;
-          this.BuildFolder = '../build'
-          this.Port = +PORT
+    if (BLAISE_API_URL === undefined) {
+      throw new ReferenceError('BLAISE_API_URL was not found in environment variables');
     }
+
+    if (BLAISE_API_URL.trim() === '') {
+      throw new Error('BLAISE_API_URL is set to an empty string');
+    }
+
+    if (PORT === undefined) {
+      throw new ReferenceError('PORT was not found in environment variables');
+    }
+
+    if (Number.isNaN(+PORT) || +PORT === 0) {
+      throw new TypeError('PORT is not set to a valid number');
+    }
+
+    if (SERVER_PARK === undefined) {
+      throw new ReferenceError('SERVER_PARK was not found in environment variables');
+    }
+
+    if (SERVER_PARK.trim() === '') {
+      throw new Error('SERVER_PARK is set to an empty string');
+    }
+
+    if (VM_EXTERNAL_WEB_URL === undefined) {
+      throw new ReferenceError('VM_EXTERNAL_WEB_URL was not found in environment variables');
+    }
+
+    if (VM_EXTERNAL_WEB_URL.trim() === '') {
+      throw new Error('VM_EXTERNAL_WEB_URL is set to an empty string');
+    }
+
+    this.BlaiseApiUrl = BLAISE_API_URL;
+    this.BuildFolder = '../build';
+    this.Port = +PORT;
+    this.ServerPark = SERVER_PARK;
+    this.ExternalWebUrl = VM_EXTERNAL_WEB_URL;
+  }
 }
