af6cf5c2e0b70c8b435dae21d1a734d5e7f478f0
TKeable <toby.keable@ons.gov.uk>
2023-10-03 13:55:32 +0100

    updated

--- src/server/api/OpenAiApi.ts
+++ src/server/api/OpenAiApi.ts
@@ -12,16 +12,17 @@ export default class OpenAiApi {
   /* eslint-disable no-console,  class-methods-use-this */
   async getResponse(): Promise<string> {
     const filePath = `${__dirname}\\..\\..\\data`;
-    const file = `${filePath}\\median_household_income_time_series_year_2021.json`;
+    const dataFile = `${filePath}\\consumer_price_inflation_time_series_3009232.jsonl`;
     console.debug('filePath ', filePath);
-   const response = await this.openAi.files.create({ file: fs.createReadStream(file), purpose: 'fine-tune' });
+   const response = await this.openAi.files.create({ file: fs.createReadStream(dataFile), purpose: 'fine-tune' });
     console.debug(response); 
-    const chatCompletion = await this.openAi.chat.completions.create({
+/*     const chatCompletion = await this.openAi.chat.completions.create({
       messages: [{ role: 'user', content: 'what is the was the inflation rate in the UK before 2021' }],
       model: 'gpt-3.5-turbo',
-    });
+    }); */
 
-    console.log(chatCompletion.choices);
-    return chatCompletion.choices[0]?.message.content as string;
+    //console.log(chatCompletion.choices);
+    //return chatCompletion.choices[0]?.message.content as string;
+    return 'yo';
   }
 }
